193516119ebe991dbd5c1f859710823290043a40
Gu5tavoGallardo <g_gallargo01@icloud.com>
2024-11-29 22:42:27 -0600

    this took me 8 hours

---
+++ my-app/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import LoginPage from "./components/LoginPage"; // Adjusted path to components directory
+
+function App() {
+  return (
+    <div>
+      <LoginPage />
+    </div>
+  );
+}
+
+export default App;
+
---
+++ my-app/src/config/firebase.js
@@ -0,0 +1,33 @@
+import { initializeApp } from "firebase/app";
+import { 
+  getAuth, 
+  GoogleAuthProvider, 
+  signInWithPopup, 
+  signInWithEmailAndPassword, 
+  createUserWithEmailAndPassword, 
+  signOut 
+} from "firebase/auth";
+
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBxNAVgflN_edduW7qXReKQ2Fw9o6AolAw",
+  authDomain: "final-like-for-reals.firebaseapp.com",
+  projectId: "final-like-for-reals",
+  storageBucket: "final-like-for-reals.firebasestorage.app",
+  messagingSenderId: "113264564781",
+  appId: "1:113264564781:web:430390119c2a2fbf6d76a4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
+
+export { 
+  auth, 
+  googleProvider, 
+  signInWithPopup, 
+  signInWithEmailAndPassword, 
+  createUserWithEmailAndPassword, 
+  signOut 
+};
