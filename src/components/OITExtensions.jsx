066059363704361bc669ffb23cbcc921a255cd90
Gu5tavoGallardo <g_gallargo01@icloud.com>
2024-12-04 22:58:44 -0600

    ok hope this works

--- src/components/OITExtensions.jsx
+++ src/components/OITExtensions.jsx
@@ -7,9 +7,9 @@ function OITExtensions({ setCurrentPage }) {
     <div className="extensions-container">
       <h1>OIT Extension Numbers</h1>
       <p className="extension-info">
-        <strong>Help Desk (956)-326-2310</strong> <br />
-        <strong>E-Learning (956)-326-2792</strong> <br />
-        <strong>Visual Audio (956)-326-2017</strong>
+        <strong>Help Desk 956-326-2310</strong> <br />
+        <strong>E-Learning 956-326-2792</strong> <br />
+        <strong>Visual Audio 956-326-2017</strong>
       </p>
       <button className="button" onClick={() => setCurrentPage('home')}>Back to Home</button>
     </div>
